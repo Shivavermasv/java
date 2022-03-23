71bbbd193dbe0eb68573647852a75124094c61c3
shivavermasv <1000015249@dit.edu.in>
2022-03-23 21:48:48 +0530

    aman

---
+++ atm.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class atm{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("INPUT THYE AMNT TO BE WITHDRAWL: ");
+        int x=sc.nextInt();
+        double y=sc.nextDouble();
+        if(x%5==0){
+            System.out.println(String.format("%.2f",  y-(x+0.50)));
+        }
+        else{
+            System.out.println("invalid transaction");
+        }
+    }
+}
